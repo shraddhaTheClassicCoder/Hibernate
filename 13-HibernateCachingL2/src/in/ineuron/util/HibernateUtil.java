66d4b1b3c55365e55f769d74d79b18acc700bfe9
shraddhaTheClassicCoder <shraddha.sahane23@gmail.com>
2023-11-05 20:12:43 +0530

    Commiting project of L2 Cache

---
+++ 13-HibernateCachingL2/src/in/ineuron/util/HibernateUtil.java
@@ -0,0 +1,42 @@
+package in.ineuron.util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import in.ineuron.model.InsurancePolicy;
+
+public class HibernateUtil {
+
+	private static SessionFactory sessionFactory = null;
+	private static Session session = null;
+
+	private HibernateUtil() {
+		// Making constructor private so no one can create object of it outside of the
+		// class
+	}
+
+	static {
+		sessionFactory = new Configuration().configure().addAnnotatedClass(InsurancePolicy.class).buildSessionFactory();
+
+	}
+
+	public static Session getSession() {
+		if (session == null) {
+			session = sessionFactory.openSession();
+
+		}
+		return session;
+	}
+
+	public static void closeUp(Session session, SessionFactory sessionFactory) {
+
+		if (session != null) {
+			session.close();
+		}
+		if (sessionFactory != null) {
+			sessionFactory.close();
+		}
+	}
+
+}
